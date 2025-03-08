d3fa11c8eba260da93cf4fb9d0599f886de636c9
JesusVal <jesusvaladezf@outlook.com>
2025-03-08 00:53:43 -0600

    Favicon Added

--- _config.ts
+++ _config.ts
@@ -2,10 +2,8 @@ import lume from "lume/mod.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import sass from "lume/plugins/sass.ts";
-//import terser from "lume/plugins/terser.ts";
 import esbuild from "lume/plugins/esbuild.ts";
-//import * as THREE from "npm:three";
-//import {THREE} from "https://cdnjs.cloudflare.com/ajax/libs/three.js/99/three.min.js";
+import favicon from "lume/plugins/favicon.ts";
 
 const site = lume(
   {
@@ -22,11 +20,8 @@ site.use(nunjucks(/* Options */));
 site.use(code_highlight(/* Options */));
 site.use(sass(/* Options */));
 site.use(esbuild(/* Options */));
-/*
-site.use(terser({
-  options: {
-    module: true,
-  },
+site.use(favicon({
+  input: "/images/icons8-console-50.png",
 }));
-*/
+
 export default site;
